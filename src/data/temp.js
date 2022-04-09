083f79784903f34e13deb54b743e8b45073ae8a0
zombie9 <darryl.banks@zombie9.com>
2022-04-09 13:05:54 +0100

    update chars

---
+++ src/data/temp.js
@@ -0,0 +1,124 @@
+const myriad = {
+  persona: {
+    name: 'ID [Illusionist / Drug]',
+    race: 'Human',
+    class: 'Magician (Space Hippy)',
+    affiliation: 'Zero State',
+    description: 'Shoulder-length brown hair\nCrochetted, long, grey cardigan\nDead Kennedys \'Police Truck\' t-shirt\nGrey jeans\nBlack converse high-tops'
+  },
+  stats: {
+    karma: '677',
+    strength: '12',
+    life: '677',
+    intelligence: '18',
+    sanity: '3',
+    dexterity: '12',
+    experience: '76'
+  },
+  skills: [
+    {
+      name: 'Perception',
+      stat: 'intelligence',
+      description: 'The ability to notice stuff',
+      value: '8'
+    },
+    {
+      name: 'Drug Component',
+      stat: 'intelligence',
+      description: 'Enhances spells with drug use. Components need to be prepared.\nSpell roll provides bonus: 20: +1, 25: +2, 30: +3, 35: +4, 40: +5',
+      value: '11'
+    },
+    {
+      name: 'Illusion',
+      stat: 'intelligence',
+      description: 'Visual, Audial, Orlfactory and Touch',
+      value: '10'
+    },
+    {
+      name: 'Commune',
+      stat: 'intelligence',
+      description: 'Divination ability.\nRitual (1 hour) involves blood.\nRitual (10 mins): -3\nInstant: -6',
+      value: '9'
+    },
+    {
+      name: 'Mindphish',
+      stat: 'intelligence',
+      description: 'Strong suggestion (does not deal damage)',
+      value: '10'
+    },
+    {
+      name: 'Phase Out',
+      stat: 'intelligence',
+      description: 'Shifts out of phase to avoid physical harm',
+      value: '10'
+    },
+    {
+      name: 'Intellect Fortress',
+      stat: 'intelligence',
+      description: 'Blocks psychic attacks and abilities',
+      value: '9'
+    },
+    {
+      name: 'Tentacles of Pnarkotis',
+      stat: 'intelligence',
+      description: 'Sends black tentacles from his chest to attack (20\' range)\nNumber of tentacles = how much they hit by (max 12)\nEach tentacle does D12 damage',
+      value: '9'
+    },
+    {
+      name: 'Sanctuary',
+      stat: 'intelligence',
+      description: 'Pulls ID and companions (-1 per extra character) to Stuart Island',
+      value: '10'
+    },
+    {
+      name: 'Exploit Reality',
+      stat: 'intelligence',
+      description: 'Change events after they occur - may incur penalties depending on severity of the change',
+      value: '10'
+    },
+    {
+      name: 'Mend',
+      stat: 'intelligence',
+      description: 'Undoes the last damage to the target and adds 3D12 Life',
+      value: '4'
+    },
+    {
+      name: 'Computer programming / systems knowledge',
+      stat: 'intelligence',
+      description: 'Includes hacking.',
+      value: '8'
+    },
+    {
+      name: 'French',
+      stat: 'intelligence',
+      description: 'Language',
+      value: '3'
+    },
+    {
+      name: 'Pnarkotic',
+      stat: 'intelligence',
+      description: 'Language',
+      value: '3'
+    },
+    {
+      name: 'Oerik',
+      stat: 'intelligence',
+      description: 'Language',
+      value: '1'
+    },
+    {
+      name: 'Bludgeon',
+      stat: 'strength',
+      description: 'Melee: One-handed blunt instruments',
+      value: '2'
+    },
+    {
+      name: 'Hex Chamber Navigation',
+      stat: 'intelligence',
+      description: 'Knowledge of hex chamber mechanics',
+      value: '3'
+    }
+  ],
+  equipment: 'Book of Morgant - demonology bonus ? - astral travel protection (human skin binding) - time boomerang - curse you (name) - last page ... wish - ritual (blood writing)\n\nCODEX\n\nBook of Pnarkosis\n1 Tentacles of Pnarkotis (known) …. D12 Tentacles (independent) d12 damage each\n2 Pnarkotis Pentagon\n3-10 (unknown)\n\nKnife (for drawing blood in rituals)\nMultiple drugs, herbs and ingredients for spell components\nBaseball bat\nSatchel - holds Drugs, Book of Pnarkotis and other equipment\nCardigan\n',
+  notes: 'Behind a mirror in the Rotten Frog. They broke it and I step into the room.\nMana: 677 -> 668 -> 658 -> 643 -> 634 -> 630 -> 621 -> 612\nDormant memory:\nUCT system - Universal Classification Types\n- Universe: \nlevel 4 - everything we have experienced /logically possible Goldilocks zone \n - 26 catageories - combinations of tags - A to Z - eg. HIJ - H = Hub, I = Imperium (empire civilisation), J = Junction\nlevel 3 - alien\nlevel 2 - logically possible but uninhabitable\nlevel 1 - is impossible, self defeating\n\nOmar - we will meet again? He is a friend. Apology? Dragged into his shit in Kosovo.\n- Universe being actively worked on\n- Driver in the tank (picture) real guy / real picture. Google this: "kosovo 1999 save the world musician".\n- Omar = is Lord Omar. \n\n1000 GP Jewelery\n\nTreasure:\n2002 Gold coins\n427 Silver coins\n4012 Copper coins\n\nTina:\nCopy of Naked Lunch -> highlighted message = 2 magical spells\nRing of Sustanence\n\nDarryl:\nPotion of Extra Healing d4 dose => max life\nBook. 2 spells => to be decided\n\n'
+};
