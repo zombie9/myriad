28c5720413fe9950d2cd8403c3ff0f9856bf056c
zombie9 <darryl.banks@zombie9.com>
2023-09-16 14:59:24 +0100

    remove unsubscribe function

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -10,38 +10,35 @@ export const useFirebase = (currentUser) => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(null);
 
-  useEffect(() => {
-    const unsubscribe = async () => {
-      setLoading('Loading...');
-      setError(null);
-      setCharacterList(null);
-      try {
-        const currentUserId = currentUser.uid;
-        const collectionRef = await collection(db, 'characters');
-        const q = query(collectionRef, where('userId', '==', currentUserId));
-        const snap = await getDocs(q);
-        console.log('snap', snap);
-        let list = [];
-        snap.forEach((doc) => {
-          list.push({
-            id: doc.id,
-            ...doc.data()
-          });
+  useEffect(async () => {
+    setLoading('Loading...');
+    setError(null);
+    setCharacterList(null);
+    try {
+      const currentUserId = currentUser.uid;
+      const collectionRef = await collection(db, 'characters');
+      const q = query(collectionRef, where('userId', '==', currentUserId));
+      const snap = await getDocs(q);
+      console.log('snap', snap);
+      let list = [];
+      snap.forEach((doc) => {
+        list.push({
+          id: doc.id,
+          ...doc.data()
         });
-        if (list.length < 1) {
-          console.log('no characters!');
-          setError('Could not retrieve any saved characters.');
-          return;
-        }
-        console.log('list', list);
-        setCharacterList(list);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-        setError('Fetch error.');
+      });
+      if (list.length < 1) {
+        console.log('no characters!');
+        setError('Could not retrieve any saved characters.');
+        return;
       }
-    };
-    return unsubscribe;
+      console.log('list', list);
+      setCharacterList(list);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setError('Fetch error.');
+    }
   }, []);
 
   return { characterList, loading, error };
